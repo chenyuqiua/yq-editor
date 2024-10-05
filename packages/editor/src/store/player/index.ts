3dc7e3bd429d12a42fa74e35f62df8e76acceaab
dengzhengcheng <lananabc@163.com>
2024-10-05 18:50:06 +0800

    feat: change store

--- packages/editor/src/store/player/index.ts
+++ packages/editor/src/store/player/index.ts
@@ -1,9 +1,15 @@
-import { shallow } from "zustand/shallow";
-import { playerSlice, PlayerSliceType } from "./slice";
+import { combine, devtools } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
 import { createWithEqualityFn } from "zustand/traditional";
+import { shallow } from "zustand/shallow";
+import { PlayTargetTrackMap } from "../type";
 
-type StoreType = PlayerSliceType;
-export const usePlayerStore = createWithEqualityFn<StoreType>()(
-  playerSlice,
+const initialState = {
+  playTargetTrackMap: new Map() as PlayTargetTrackMap, // 当前正确播放的元素的集合
+  playStartFrame: 0, // 当前播放帧
+};
+
+export const usePlayerStore = createWithEqualityFn(
+  devtools(immer(combine(initialState, (set, get) => ({})))),
   shallow
 );
--- packages/editor/src/store/track-attribute/index.ts
+++ packages/editor/src/store/track-attribute/index.ts
@@ -1,9 +1,16 @@
-import { shallow } from "zustand/shallow";
-import { trackAttributeSlice, TrackAttributeType } from "./slice";
+import { localStorageJsonGet, LocalStorageKeyEnum } from "@/util/local-storage";
+import type { TrackAttributeMap } from "../type";
 import { createWithEqualityFn } from "zustand/traditional";
+import { combine, devtools } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
+import { shallow } from "zustand/shallow";
 
-type StoreType = TrackAttributeType;
-export const useTrackAttributeStore = createWithEqualityFn<StoreType>()(
-  trackAttributeSlice,
+const initialState = {
+  trackAttributeMap: (localStorageJsonGet(LocalStorageKeyEnum.TrackAttribute) ??
+    {}) as TrackAttributeMap, // 轨道上所有元素的集合 value记录着元素的属性
+};
+
+export const useTrackAttributeStore = createWithEqualityFn(
+  devtools(immer(combine(initialState, (set, get) => ({})))),
   shallow
 );
